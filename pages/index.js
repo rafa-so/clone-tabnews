f110b364b068540ce8a689cc6b9b6c0bf5e40083
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2023-07-04 14:24:38 +0000

    motivacional messge

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Teste</h1>
+    return <h1>Minha linda, você é braba demais. Esta tela faz parte do curso que estou fazendo. E espero com esta tela simples, impactar um pouco a sua vida, apesar de que você impacta muito mais a minha. Te amo.</h1>
 }
 
 export default Home;
