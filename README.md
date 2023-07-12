13498f388ab805785e3b4f2d67543fa6044bfe87
Rafael Oliveira <rafa.forsoft.25@gmail.com>
2023-07-12 13:51:14 +0000

    ajustado formatação de arquivos

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto clone de outro projeto, o tabnews. Conteúdo para desenvolvedores e para quem trabalha com tecnologia
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Minha linda, você é braba demais. Esta tela faz parte do curso que estou fazendo. E espero com esta tela simples, impactar um pouco a sua vida, apesar de que você impacta muito mais a minha. Te amo.</h1>
+  return (
+    <h1>
+      Minha linda, você é braba demais. Esta tela faz parte do curso que estou
+      fazendo. E espero com esta tela simples, impactar um pouco a sua vida,
+      apesar de que você impacta muito mais a minha. Te amo.
+    </h1>
+  );
 }
 
 export default Home;
